2f3b7fe30695613d3ed36c682f5cec484e5b11f6
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-03-21 20:53:00 +1300

    Created a module for the rest of React components

---
+++ src/components/SortBy.jsx
@@ -0,0 +1,15 @@
+
+const SortBy = () => {
+    return (
+      <div className="sortBy">
+        <label htmlFor="sort">Sort by</label>
+        <select id="sort" defaultValue="latest">
+          <option value="latest">Latest</option>
+          <option value="low">Price: Low</option>
+          <option value="high">Price: High</option>
+        </select>
+      </div>
+    );
+  };
+
+  export default SortBy;
